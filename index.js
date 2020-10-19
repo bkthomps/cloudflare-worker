785f66ab10533ee36844d413a2e87fb0667bce08
bkthomps <bkthomps@uwaterloo.ca>
2020-10-19 01:45:15 -0400

    Add JSON Response

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const url = new URL(request.url);
+  const uri = url.pathname;
+  if (uri === "/links") {
+    return handleJson();
+  }
+  return handleHtml();
+}
+
+async function handleJson() {
+  const links = {
+    github: { "name": "GitHub", "url": "https://github.com/bkthomps" },
+    linkedin: { "name": "LinkedIn", "url": "https://www.linkedin.com/in/bk-thompson/" },
+    angellist: { "name": "AngelList", "url": "https://angel.co/u/bkthomps" }
+  };
+  const response = JSON.stringify(links, null, 2);
+  return new Response(response, {
+    headers: { "content-type": "application/json" }
+  });
+}
+
+async function handleHtml() {
+  return new Response("HTML Response", {
+    headers: { "content-type": "text/html" }
+  });
 }
