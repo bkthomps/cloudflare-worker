1bc3c5707d950d252cd22d44118fa45c07713ea9
bkthomps <bkthomps@uwaterloo.ca>
2020-10-26 22:38:12 -0400

    Add HTML response

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const links = [
+  { "name": "GitHub", "url": "https://github.com/bkthomps" },
+  { "name": "LinkedIn", "url": "https://www.linkedin.com/in/bk-thompson/" },
+  { "name": "AngelList", "url": "https://angel.co/u/bkthomps" }
+];
+
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
 });
@@ -12,11 +18,6 @@ async function handleRequest(request) {
 }
 
 async function handleJson() {
-  const links = {
-    github: { "name": "GitHub", "url": "https://github.com/bkthomps" },
-    linkedin: { "name": "LinkedIn", "url": "https://www.linkedin.com/in/bk-thompson/" },
-    angellist: { "name": "AngelList", "url": "https://angel.co/u/bkthomps" }
-  };
   const response = JSON.stringify(links, null, 2);
   return new Response(response, {
     headers: { "content-type": "application/json" }
@@ -24,7 +25,19 @@ async function handleJson() {
 }
 
 async function handleHtml() {
-  return new Response("HTML Response", {
-    headers: { "content-type": "text/html" }
-  });
+  const staticPage = await fetch("https://static-links-page.signalnerve.workers.dev");
+  return new HTMLRewriter()
+    .on("div#links", new LinksTransformer())
+    .on("img#avatar", { element: e => e.setAttribute("src", "https://assets.foxdcg.com/dpp-uploaded/images/cosmos-possible-worlds/keyart_s2cosmo.jpg") })
+    .on("h1#name", { element: e => e.setInnerContent("Bailey Thompson") })
+    .transform(staticPage);
+}
+
+class LinksTransformer {
+  element(element) {
+    links.forEach(function(link) {
+      const entry = "<a href=\"" + link.url + "\">" + link.name + "</a>";
+      element.append(entry, { html: true });
+    });
+  }
 }
